618d87f9930e4a70c7aaa29889f0b663f3dbe6f5
maaz.hassan <h.maaz809@hotmail.com>
2021-03-17 22:59:00 -0600

    Even more bugfixes.

--- src/BanknotePaymentTest.java
+++ src/BanknotePaymentTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import org.lsmr.selfcheckout.*;
 import org.lsmr.selfcheckout.devices.DisabledException;
 import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
-import org.lsmr.selfcheckout.devices.listeners.BanknoteSlotListener;
 import org.lsmr.selfcheckout.external.ProductDatabases;
 import org.lsmr.selfcheckout.products.BarcodedProduct;
 import java.io.ByteArrayOutputStream;
@@ -20,8 +19,7 @@ import static org.junit.Assert.*;
 * @author <Authors Irtaza>
 * @since <pre>Mar. 17, 2021</pre> 
 * @version 1.0 
-*/ 
-
+*/
 public class BanknotePaymentTest {
     private final Currency currency = Currency.getInstance("CAD");
     private final int[] banknoteDenominations = {5, 10, 20, 50, 100};
@@ -41,93 +39,127 @@ public class BanknotePaymentTest {
         ProductDatabases.BARCODED_PRODUCT_DATABASE.clear();
     }
 
+    /**
+    *
+    * Method: validBanknoteDetected(BanknoteValidator validator, Currency currency, int value)
+    *
+    */
+    @Test
+    public void testValidBanknote() throws Exception {
+        int failCounter = 0;
+        for (int i = 0; i < 5; i++) {
+            ControlSoftware cs = new ControlSoftware();
+            BanknoteSlotController slotController = new BanknoteSlotController();
+            BanknoteValidatorController validatorController = new BanknoteValidatorController(cs);
+            BanknotesStorageUnitController storageController = new BanknotesStorageUnitController();
+            SelfCheckoutStation station = new SelfCheckoutStation(currency, banknoteDenominations, coinDenominations, 100, 1);
+            station.banknoteInput.register(slotController);
+            station.banknoteValidator.register(validatorController);
+            station.banknoteStorage.register(storageController);
+            Barcode barcode = new Barcode("123456789");
+            cs.increaseTotal(barcode);
+            Banknote fiveDollars = new Banknote(5, currency);
+            try {
+                station.banknoteInput.accept(fiveDollars);
+            }
+            catch (DisabledException de) {
+                System.out.println("Disabled.");
+            }
+            try {
+                assertEquals(cs.getTotal(), new BigDecimal("95.10"));
+                assertEquals(station.banknoteStorage.getBanknoteCount(), 1);
+            }
+            catch (AssertionError ae) {
+                failCounter++;
+                if (failCounter > 2) throw new AssertionError();
+            }
+        }
     }
-/**
-* 
-* Method: validBanknoteDetected(BanknoteValidator validator, Currency currency, int value) 
-* 
-*/
 
-@Test
-public void testValidBanknoteDetected() throws Exception {
-    int failCounter = 0;
-    for (int i = 0; i < 5; i++) {
-        BanknoteSlotController slotController = new BanknoteSlotController();
-        BanknoteValidatorController validatorController = new BanknoteValidatorController(cs);
-        BanknoteStorageController storageController = new BanknoteStorageController();
-        SelfCheckoutStation station = new SelfCheckoutStation(currency, banknoteDenominations, coinDenominations, 100, 1);
-        station.BanknoteSlot.register(slotController);
-        station.BanknoteValidator.register(validatorController);
-        station.BanknoteStorage.register(storageController);
-        Barcode barcode = new Barcode("123456789");
-        cs.increaseTotal(barcode);
-        Banknote fiveDollars = new Banknote(new BigDecimal("5.00"), currency);
-        try {
-            station.BanknoteSlot.accept(fiveDollars);
-        }
-        catch (DisabledException de) {
-            System.out.println("Disabled.");
-        }
-        try {
-            assertEquals(getTotal(), new BigDecimal("98.10"));
-            assertEquals(station.banknoteStorage.getCoinCount(), 1);
-        }
-        catch (AssertionError ae) {
-            failCounter++;
-            if (failCounter > 2) throw new AssertionError();
+    /**
+    *
+    * Method: invalidBanknoteDetected(BanknoteValidator validator)
+    *
+    */
+    @Test
+    public void testInvalidBanknoteValue() throws Exception {
+        int failCounter = 0;
+        for (int i = 0; i < 5; i++) {
+            ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+            PrintStream originalOut = System.out;
+            System.setOut(new PrintStream(outContent));
+            ControlSoftware cs = new ControlSoftware();
+            BanknoteSlotController slotController = new BanknoteSlotController();
+            BanknoteValidatorController validatorController = new BanknoteValidatorController(cs);
+            BanknotesStorageUnitController storageController = new BanknotesStorageUnitController();
+            SelfCheckoutStation station = new SelfCheckoutStation(currency, banknoteDenominations, coinDenominations, 100, 1);
+            station.banknoteInput.register(slotController);
+            station.banknoteValidator.register(validatorController);
+            station.banknoteStorage.register(storageController);
+            Barcode barcode = new Barcode("123456789");
+            cs.increaseTotal(barcode);
+            Banknote invalidBanknote = new Banknote(11, currency);
+            try {
+                station.banknoteInput.accept(invalidBanknote);
+            }
+            catch (DisabledException de) {
+                System.out.println("Disabled.");
+            }
+
+            try {
+                assertEquals(cs.getTotal(), new BigDecimal("100.10"));
+                assertEquals(station.banknoteStorage.getBanknoteCount(), 0);
+                assertEquals("An invalid banknote bill was inserted.", outContent.toString().split("\\r?\\n")[1]);
+                assertEquals("A banknote was ejected.", outContent.toString().split("\\r?\\n")[2]);
+            }
+            catch (AssertionError ae) {
+                failCounter++;
+                if (failCounter > 2) throw new AssertionError();
+            }
+            finally {
+                System.setOut(originalOut);
+            }
         }
     }
-}
 
-/** 
-* 
-* Method: invalidBanknoteDetected(BanknoteValidator validator) 
-* 
-*/ 
-@Test
-public void testInvalidBanknoteDetected() throws Exception {
-    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    PrintStream originalOut = System.out;
-    System.setOut(new PrintStream(outContent));
+    @Test
+    public void testInvalidBanknoteCurrency() throws Exception {
+        int failCounter = 0;
+        for (int i = 0; i < 5; i++) {
+            ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+            PrintStream originalOut = System.out;
+            System.setOut(new PrintStream(outContent));
+            ControlSoftware cs = new ControlSoftware();
+            BanknoteSlotController slotController = new BanknoteSlotController();
+            BanknoteValidatorController validatorController = new BanknoteValidatorController(cs);
+            BanknotesStorageUnitController storageController = new BanknotesStorageUnitController();
+            SelfCheckoutStation station = new SelfCheckoutStation(currency, banknoteDenominations, coinDenominations, 100, 1);
+            station.banknoteInput.register(slotController);
+            station.banknoteValidator.register(validatorController);
+            station.banknoteStorage.register(storageController);
+            Barcode barcode = new Barcode("123456789");
+            cs.increaseTotal(barcode);
+            Banknote invalidBanknote = new Banknote(11, Currency.getInstance("USD"));
+            try {
+                station.banknoteInput.accept(invalidBanknote);
+            }
+            catch (DisabledException de) {
+                System.out.println("Disabled.");
+            }
 
-    int failCounter = 0;
-    for (int i = 0; i < 5; i++) {
-        BanknoteSlotController slotController = new BanknoteSlotController();
-        BanknoteValidatorController validatorController = new BanknoteValidatorController(cs);
-        BanknoteStorageController storageController = new BanknoteStorageController();
-        BanknoteTrayController trayController = new BanknoteTrayController();
-        SelfCheckoutStation station = new SelfCheckoutStation(currency, banknoteDenominations, coinDenominations, 100, 1);
-        station.BanknoteSlot.register(slotController);
-        station.BanknoteValidator.register(validatorController);
-        station.BanknoteStorage.register(storageController);
-        station.BanknoteTray.register(trayController);
-        Barcode barcode = new Barcode("123456789");
-        cs.increaseTotal(barcode);
-        Banknote invalidBanknote = new Banknote(new BigDecimal("10.00"), currency);
-        try {
-            station.BanknoteSlot.accept(invalidBanknote);
+            try {
+                assertEquals(cs.getTotal(), new BigDecimal("100.10"));
+                assertEquals(station.banknoteStorage.getBanknoteCount(), 0);
+                assertEquals("An invalid banknote bill was inserted.", outContent.toString().split("\\r?\\n")[1]);
+                assertEquals("A banknote was ejected.", outContent.toString().split("\\r?\\n")[2]);
+            }
+            catch (AssertionError ae) {
+                failCounter++;
+                if (failCounter > 2) throw new AssertionError();
+            }
+            finally {
+                System.setOut(originalOut);
+            }
         }
-        catch (DisabledException de) {
-            System.out.println("Disabled.");
-        }
-
-        try {
-            assertEquals(cs.getTotal(), new BigDecimal("100.00"));
-            assertEquals(station.BanknoteStorage.getCoinCount(), 0);
-            assertEquals("A coin was added to the coin tray.", outContent.toString().split("\\r?\\n")[2]);
-        }
-        catch (AssertionError ae) {
-            failCounter++;
-            if (failCounter > 2) throw new AssertionError();
-        }
-        finally {
-            System.setOut(originalOut);
-        }
-    }}
-
-/** 
-* 
-* Method: enabled(AbstractDevice<? extends AbstractDeviceListener> device) 
-* 
-*/ 
+    }
 }
